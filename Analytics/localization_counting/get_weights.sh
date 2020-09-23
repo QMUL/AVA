04facb8c9782c62a409bbee1f732d25c2b216563
Ricardo <ricardosm89@gmail.com>
2020-09-23 09:08:10 +0100

    get_weights

--- Analytics/localization_counting/get_weights.sh
+++ Analytics/localization_counting/get_weights.sh
@@ -2,17 +2,21 @@
 cd retinaface
 wget http://test-cyl.eecs.qmul.ac.uk/ava/A1.zip
 unzip A1.zip
+rm A1.zip
 
 cd ../mtcnn
 wget http://test-cyl.eecs.qmul.ac.uk/ava/A2.zip
 unzip A2.zip
+rm A2.zip
 
 cd ../deepsort
 wget http://test-cyl.eecs.qmul.ac.uk/ava/A3.zip
 unzip A3.zip
+rm A3.zip
 
 cd ../trmot
 wget http://test-cyl.eecs.qmul.ac.uk/ava/A4.zip
 unzip A4.zip
+rm A4.zip
 
 cd ..
