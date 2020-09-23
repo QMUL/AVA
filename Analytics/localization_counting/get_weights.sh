88d83daca0418a508fb0c74602507b472baff3cf
Ricardo <ricardosm89@gmail.com>
2020-09-23 08:41:59 +0100

    improved scripts

---
+++ Analytics/localization_counting/get_weights.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd retinaface
+wget http://test-cyl.eecs.qmul.ac.uk/ava/A1.zip
+unzip A1.zip
+
+cd ../mtcnn
+wget http://test-cyl.eecs.qmul.ac.uk/ava/A2.zip
+unzip A2.zip
+
+cd ../deepsort
+wget http://test-cyl.eecs.qmul.ac.uk/ava/A3.zip
+unzip A3.zip
+
+cd ../trmot
+wget http://test-cyl.eecs.qmul.ac.uk/ava/A4.zip
+unzip A4.zip
+
+cd ..
